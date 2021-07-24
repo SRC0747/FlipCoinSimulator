ef074c8a87d010816f90c591c64d5845c4f4605c
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 03:49:27 +0530

    Checking if Tie till the game will be continued till the minimum 2 points is achiieved.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -23,6 +23,19 @@ then
         echo "Tail wins by $(($tail_value-$head_value))"
 else
 	echo "Tie"
+	if [ $isCheck -eq $randomCheck ]
+        then
+                head_value=$(($head_value+1))
+        else
+                tail_value=$(($tail_value+1))
+        fi
+	if [ $(($head_value-$tail_value)) -ge 2 ]
+	then
+		echo "Head wins  by 2."
+	elif [ $(($tail_value-$head_value)) -ge 2 ]
+        then
+                echo "Tail wins  by 2."
+	fi
 fi
 
 
