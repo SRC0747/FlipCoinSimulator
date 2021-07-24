a8d94568154b157021ea74062c1c58cde81d8cac
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 03:36:17 +0530

    Checking for multiple times head or tail won.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to FlipCoin Simulator problem."
 isCheck=1
+head_value=0
+tail_value=0
 randomCheck=$((RANDOM%2))
-if [ $isCheck -eq $randomCheck ]
+count=1
+while [ $count -le 21 ]
+do
+	if [ $isCheck -eq $randomCheck ]
+	then
+		head_value=$(($head_value+1))
+	else
+		tail_value=$(($tail_value+1))
+	fi
+	((count++))
+done
+if [ $head_value -gt $tail_value ]
 then
-	echo "Head value"
-else
-	echo "Tail value."
+	echo "Head wins by $(($head_value-$tail_value))"
+elif [ $tail_value -gt $head_value ]
+then
+        echo "Tail wins by $(($tail_value-$head_value))"
 fi
+
+
