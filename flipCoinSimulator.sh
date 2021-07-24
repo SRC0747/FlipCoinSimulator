65de804179ca2077f6c83621ad4f72e765dbd52a
SRC0747 <srcsrk1998@gmail.com>
2021-07-25 03:29:06 +0530

    Checking head or tail value by ((RANDOM))

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,2 +1,10 @@
 #!/bin/bash -x
 echo "Welcome to FlipCoin Simulator problem."
+isCheck=1
+randomCheck=$((RANDOM%2))
+if [ $isCheck -eq $randomCheck ]
+then
+	echo "Head value"
+else
+	echo "Tail value."
+fi
